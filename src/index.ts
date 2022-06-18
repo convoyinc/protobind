7152815e2fa53323eeec31a81704eb3b744f3072
John Daly <john.daly2@gmail.com>
2022-06-18 11:26:56 -0700

    - Removing usage of lodash - Updating code to use more modern TS syntax

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,34 @@
-import * as _ from 'lodash';
-
-interface Class {
-  new(...args:any[]):Class;
-}
-
 /**
  * If `target` is a class, a subclass is returned that binds all methods on its
  * instances to themselves.  Otherwise, binds all methods exposed on `target`,
  * including those that come from its prototype(s).
  */
-function protobind(target:any):any {
-  if (_.isFunction(target)) {
-    class Autobound extends (<Class>target) {
-      constructor(...args:any[]) {
+export function protobind(target: any): any {
+  // Support use of @protobind decorator
+  if (typeof target === 'function') {
+    class Autobound extends (target as ObjectConstructor) {
+      constructor(...args: any[]) {
         super(...args);
         _autobind(this);
       }
     }
     return Autobound;
-  } else {
-    _autobind(target);
-    return target;
   }
+
+  // Standard usage of protobind() function
+  _autobind(target);
+  return target;
 }
-namespace protobind {}
-export = protobind;
 
 /**
  * Autobinds all functions present in `source` to `target`, and walks up the
  * prototype chain of `source`.
  */
-function _autobind(target:{}):void {
-  const names:{[key:string]:boolean} = {};
+function _autobind(target: Object): void {
+  const names = {};
   _fillBindablePropertyNames(target, names);
 
-  for (const key of _.keys(names)) {
+  for (const key of Object.keys(names)) {
     target[key] = _bind(target, target[key]);
   }
 }
@@ -43,10 +37,10 @@ function _autobind(target:{}):void {
  * Collect a flat set of all property names on `target` that should be bound,
  * by walking up its prototype chain.
  */
-function _fillBindablePropertyNames(target:{}, names:{[key:string]:boolean}):void {
+function _fillBindablePropertyNames(target: Object, names: Record<string, boolean>): void {
   for (const key of Object.getOwnPropertyNames(target)) {
     if (key === 'constructor') continue;
-    if (!_.isFunction(target[key])) continue;
+    if (typeof target[key] !== 'function') continue;
     names[key] = true;
   }
   const nextTarget = Object.getPrototypeOf(target);
@@ -59,8 +53,8 @@ function _fillBindablePropertyNames(target:{}, names:{[key:string]:boolean}):voi
  * Bind `method` to `target`, preserving any (enumerable) properties present on
  * `method`.
  */
-function _bind<T>(target:{}, method:T):T {
-  const bound = (<any>method).bind(target);
+function _bind<T extends Function>(target: Object, method: T): T {
+  const bound = method.bind(target);
   for (const name in method) {
     bound[name] = method[name];
   }
